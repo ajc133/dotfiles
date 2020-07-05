05791e4abb220dfd877020a8cddc92d7ab011d98
AJ C <16407457+ajc133@users.noreply.github.com>
2020-07-05 16:51:11 -0700

    Install only if files don't already exist

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ cp zshrc ~/.zshrc
 cp -R vim ~/.vim
 cp tmux.conf ~/.tmux.conf
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
-echo "source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+grep -q "zsh-syntax-highlighting" ${ZDOTDIR:-$HOME}/.zshrc
+if [[ $? -ne 0 ]]; then
+  echo "source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
+
+if [[ ! -d ~/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
