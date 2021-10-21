d08f61b27fb98ce86b46546bf7b3d998b918ebf6
AJ Collins <16407457+ajc133@users.noreply.github.com>
2021-10-21 10:35:25 -0700

    Add package install script

---
+++ README.md
@@ -0,0 +1,3 @@
+# AJ's Dotfiles
+
+Uses [chezmoi](https://github.com/twpayne/chezmoi) to manage updates between machines
---
+++ run_once_install_packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install nmap git tmux curl zsh
+# Download nvim from https://github.com/neovim/neovim/releases, copy to /usr/local/bin/nvim, chmod +x
