c56f0780afae74daab98eb41b7cec7affe4742bd
AJ Collins <16407457+ajc133@users.noreply.github.com>
2021-10-21 18:34:44 -0700

    Install fzf idempotently

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -4,7 +4,15 @@ sudo apt update
 sudo apt install nmap git tmux curl zsh
 # Download nvim from https://github.com/neovim/neovim/releases, copy to /usr/local/bin/nvim, chmod +x
 
+vimplug_path="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim 
 # vimplug
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -f "$vimplug_path" ]; then
+    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
+# fzf
+if [ -f "$HOME/.fzf" ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+    $HOME/.fzf/install
+fi
