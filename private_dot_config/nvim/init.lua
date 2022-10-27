7fe7a1367493c85467d5e7d3c84448c1d23acfe9
AJ Collins <16407457+ajc133@users.noreply.github.com>
2022-10-27 12:01:53 -0700

    configure treesitter highlighting

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -194,8 +194,20 @@ require('telescope').setup{
 
 -- Syntax Highlighting
 require("nvim-treesitter.configs").setup {
+  ensure_installed = { "json", "javascript", "python", "rust", "lua", "go", "gomod", "bash" },
+  auto_install = false,
   highlight = {
-    -- ...
+    enable = true,
+
+    -- Disable for large files
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+
   },
   -- ...
   rainbow = {
