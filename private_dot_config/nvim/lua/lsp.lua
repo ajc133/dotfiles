b15b51d3a799c4ff9f4e79717738da1042a06178
AJ Collins <16407457+ajc133@users.noreply.github.com>
2023-01-06 13:00:55 -0800

    lsp format
    apparently lua likes tabs over spaces

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -1,101 +1,99 @@
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<Leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, bufopts)
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<Leader>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<Leader>fo', vim.lsp.buf.format, bufopts)
 end
 
 local lsp_flags = {
-  debounce_text_changes = 150,
+	debounce_text_changes = 150,
 }
 
-require('lspconfig')['sumneko_lua'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  log_level = 2,
-  -- root_dir = vim.fs.dirname(vim.fs.find( {'.luarc.json', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', '.git'}, { upward = true })[1]),
-  settings = {
-    Lua = {
-      runtime = {
-	-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-	version = 'LuaJIT',
-      },
-      diagnostics = {
-	-- Get the language server to recognize the `vim` global
-	globals = {'vim'},
-      },
-      workspace = {
-	-- Make the server aware of Neovim runtime files
-	library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-	enable = false,
-      },
-    },
-  },
+require('lspconfig')['sumneko_lua'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	log_level = 2,
+	-- root_dir = vim.fs.dirname(vim.fs.find( {'.luarc.json', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', '.git'}, { upward = true })[1]),
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 }
 
-require('lspconfig')['bashls'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
+require('lspconfig')['bashls'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
 }
 
-require('lspconfig')['gopls'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
+require('lspconfig')['gopls'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
 }
 
-require('lspconfig')['pyright'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
+require('lspconfig')['pyright'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
 }
 
-require('lspconfig')['tsserver'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
+require('lspconfig')['tsserver'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
 }
 
-require('lspconfig')['terraformls'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
-  filetypes = { "terraform", "hcl", "tf" }
+require('lspconfig')['terraformls'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
+	filetypes = { "terraform", "hcl", "tf" }
 }
 
-require('lspconfig')['rust_analyzer'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
+require('lspconfig')['rust_analyzer'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
 }
 
-require('lspconfig')['yamlls'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
-  filetypes = { "yaml", "yml" },
-  settings = {
-    validate = true;
-  }
+require('lspconfig')['yamlls'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
+	filetypes = { "yaml", "yml" },
+	settings = {
+		validate = true;
+	}
 }
-
-
--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -2,84 +2,84 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(
-  function(use)
+	function(use)
 
-    -- Get startuptime with `:StartupTime`
-    use 'dstein64/vim-startuptime'
+		-- Get startuptime with `:StartupTime`
+		use 'dstein64/vim-startuptime'
 
-    -- "Packer can manage itself"
-    use 'wbthomason/packer.nvim'
+		-- "Packer can manage itself"
+		use 'wbthomason/packer.nvim'
 
-    -- Status line
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'nvim-tree/nvim-web-devicons' },
-      config = function()
-	require('lualine').setup({
-	  sections = {
-	    lualine_a = { { 'buffers', show_filename_only = false } }
-	  }
-	})
-      end
-    }
+		-- Status line
+		use {
+			'nvim-lualine/lualine.nvim',
+			requires = { 'nvim-tree/nvim-web-devicons' },
+			config = function()
+				require('lualine').setup({
+					sections = {
+						lualine_a = { { 'buffers', show_filename_only = false } }
+					}
+				})
+			end
+		}
 
-    use {
-      'akinsho/bufferline.nvim',
-      tag = "v3.*",
-      requires = 'nvim-tree/nvim-web-devicons',
-      config = function() require('bufferline').setup{} end,
-    }
+		use {
+			'akinsho/bufferline.nvim',
+			tag = "v3.*",
+			requires = 'nvim-tree/nvim-web-devicons',
+			config = function() require('bufferline').setup {} end,
+		}
 
-    -- Syntax Highlighting
-    -- use {
-    --   'nvim-treesitter/nvim-treesitter',
-    --   run = ':TSUpdate'
-    -- }
-    -- use {
-    --   'p00f/nvim-ts-rainbow',
-    --   requires = { 'nvim-treesitter/nvim-treesitter' }
-    -- }
+		-- Syntax Highlighting
+		-- use {
+		--   'nvim-treesitter/nvim-treesitter',
+		--   run = ':TSUpdate'
+		-- }
+		-- use {
+		--   'p00f/nvim-ts-rainbow',
+		--   requires = { 'nvim-treesitter/nvim-treesitter' }
+		-- }
 
-   -- Git commands
-    use 'tpope/vim-fugitive'
-    use 'shumphrey/fugitive-gitlab.vim'
+		-- Git commands
+		use 'tpope/vim-fugitive'
+		use 'shumphrey/fugitive-gitlab.vim'
 
-    -- Colorschemes
-    use 'lifepillar/vim-gruvbox8'
-    use 'frankier/neovim-colors-solarized-truecolor-only'
-    use 'folke/tokyonight.nvim'
-    use {
-      'marko-cerovac/material.nvim',
-      config = function() require('material').setup({lualine_style = 'default'}) end,
-    }
+		-- Colorschemes
+		use 'lifepillar/vim-gruvbox8'
+		use 'frankier/neovim-colors-solarized-truecolor-only'
+		use 'folke/tokyonight.nvim'
+		use {
+			'marko-cerovac/material.nvim',
+			config = function() require('material').setup({ lualine_style = 'default' }) end,
+		}
 
 
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/mason.nvim'
+		-- LSP
+		use 'neovim/nvim-lspconfig'
+		use 'williamboman/mason.nvim'
 
-    -- Golang
-    use 'fatih/vim-go'
+		-- Golang
+		use 'fatih/vim-go'
 
-    -- Comment things out
-    use 'tpope/vim-commentary'
+		-- Comment things out
+		use 'tpope/vim-commentary'
 
-    -- Completion
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
+		-- Completion
+		use 'hrsh7th/cmp-nvim-lsp'
+		use 'hrsh7th/cmp-buffer'
+		use 'hrsh7th/cmp-path'
+		use 'hrsh7th/cmp-cmdline'
+		use 'hrsh7th/nvim-cmp'
 
-    -- Telescope
-    use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.0',
-      requires = { {'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons'} }
-    }
+		-- Telescope
+		use {
+			'nvim-telescope/telescope.nvim', tag = '0.1.0',
+			requires = { { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' } }
+		}
 
-    -- FIXME: Snippets
-    -- use 'SirVer/ultisnips'
-    -- use 'quangnguyen30192/cmp-nvim-ultisnips'
+		-- FIXME: Snippets
+		-- use 'SirVer/ultisnips'
+		-- use 'quangnguyen30192/cmp-nvim-ultisnips'
 
-  end
+	end
 )
