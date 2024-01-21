7804e652c791d672d288b91417ca5a17edea3daa
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-01-20 18:14:20 -0800

    add tool-installer

--- private_dot_config/nvim-lspzero/init.lua
+++ private_dot_config/nvim-lspzero/init.lua
@@ -156,6 +156,35 @@ require('mason-lspconfig').setup({
   },
 })
 
+require('mason-tool-installer').setup {
+  ensure_installed = {
+    -- Commented the ones I want to try in the future
+    'bash-language-server',
+    'black',
+    'editorconfig-checker',
+    'golangci-lint',
+    'golines',
+    -- 'gomodifytags',
+    'gopls',
+    -- 'gotests',
+    'html-lsp',
+    'isort',
+    'jedi-language-server',
+    -- 'json-to-struct',
+    'lua-language-server',
+    'rust-analyzer',
+    'shellcheck',
+    'typescript-language-server',
+    -- 'staticcheck',
+    'yaml-language-server',
+  },
+
+  auto_update = false,
+  run_on_start = true,
+  start_delay = 3000, -- 3 second delay
+  debounce_hours = 5, -- at least 5 hours between attempts to install/update
+}
+
 local cmp = require('cmp')
 
 cmp.setup({
