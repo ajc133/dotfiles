09854099e46b12948eb459d48bc2e373fa3e0b6f
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-04-01 08:30:38 -0700

    update brewfile

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -70,7 +70,7 @@ return {
 		config = function()
 			require("auto-session").setup({
 				log_level = "error",
-				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+				auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
 			})
 		end,
 	},
