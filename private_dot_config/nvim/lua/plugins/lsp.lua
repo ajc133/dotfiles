55cc3f64bd70ad556c678a70b0e2ab6f561247ce
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-04-05 16:29:48 -0700

    install sql-language-server

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -140,6 +140,11 @@ return {
 					},
 				},
 				rust_analyzer = {},
+				sqlls = {
+					root_dir = function()
+						return vim.loop.cwd()
+					end,
+				},
 				terraformls = {},
 				tsserver = {},
 				yamlls = {},
