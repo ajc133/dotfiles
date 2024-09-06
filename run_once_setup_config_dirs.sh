8f973d8fadcaffa625bd1069006b75924844b5d4
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-09-06 11:18:38 -0700

    move zsh setup to its own file

--- run_once_setup_config_dirs.sh
+++ run_once_setup_config_dirs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p $HOME/.config/nvim
+mkdir -p "$HOME/.config/nvim"
 
-cd "$HOME/.local/share/chezmoi" && git remote set-url origin git@github.com:ajc133/dotfiles.git
+git -C "$HOME/.local/share/chezmoi" remote set-url --push origin git@github.com:ajc133/dotfiles.git
---
+++ run_onchange_setup_zsh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# clone oh-my-zsh
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+fi
+
+function zsh_clone {
+	type="$1" # 'themes' or 'plugins'
+	plugin_url="$2"
+	plugin_name="$(printf '%s/' "$plugin_url" | awk -F '/' '{print $NF}')"
+	plugin_path="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/${type}/${plugin_name}"
+	if [ ! -d "$plugin_path" ]; then
+		git clone "$plugin_url" "$plugin_path"
+	fi
+}
+
+zsh_clone themes https://github.com/romkatv/powerlevel10k
+zsh_clone plugins https://github.com/zsh-users/zsh-autosuggestions
+zsh_clone plugins https://github.com/zsh-users/zsh-syntax-highlighting
+zsh_clone plugins https://github.com/MichaelAquilina/zsh-you-should-use.git
+zsh_clone plugins https://github.com/Aloxaf/fzf-tab
