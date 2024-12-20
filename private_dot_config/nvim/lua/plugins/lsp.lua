fa90b6c3cb831ba3d706b579dce5df6fdb8c27b6
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-12-20 10:19:40 -0800

    nvim: Switch to super-tab autocomplete

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -233,7 +233,7 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
-      keymap = { preset = 'enter' },
+      keymap = { preset = 'super-tab' },
 
       appearance = {
         use_nvim_cmp_as_default = true,
