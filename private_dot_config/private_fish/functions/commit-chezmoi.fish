76e8d677839e225aa214e6fff02a29ca18e4bc51
AJ Collins <16407457+ajc133@users.noreply.github.com>
2025-07-24 14:49:22 -0700

    fix commit-chezmoi to use 'ch git push'

--- private_dot_config/private_fish/functions/commit-chezmoi.fish
+++ private_dot_config/private_fish/functions/commit-chezmoi.fish
@@ -1,5 +1,5 @@
 function commit-chezmoi
     ch git add . && ch git commit && read --prompt-str "Push to GitHub? (y/n) " push && if test "$push" = "y"
-        git push
+        ch git push
     end
 end
