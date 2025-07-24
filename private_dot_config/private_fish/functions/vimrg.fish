86f659552f02ebef1d963c7f67d96813317a672d
AJ Collins <16407457+ajc133@users.noreply.github.com>
2025-07-24 12:20:11 -0700

    fix vimrg

--- private_dot_config/private_fish/functions/vimrg.fish
+++ private_dot_config/private_fish/functions/vimrg.fish
@@ -1,4 +1,14 @@
-function vimrg --wraps="rg -l 'vim' | xargs nvim" --description "alias vimrg=rg -l 'vim' | xargs nvim"
-  rg -l 'vim' | xargs nvim $argv
-        
+function vimrg --wraps="rg -l 'vim' | xargs nvim" --wraps='rg -l  | xargs nvim'
+    if test (count $argv) -eq 0
+        echo "Usage: vimrg <search_term>"
+        return 1
+    end
+
+    set matches (rg --files-with-matches --no-messages $argv[1])
+    if test (count $matches) -eq 0
+        echo "No matches found for '$argv[1]'"
+        return 1
+    end
+
+    nvim $matches
 end
