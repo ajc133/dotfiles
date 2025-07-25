06e156aae0b18d6433cb76170b76bd8369b8dc50
AJ Collins <16407457+ajc133@users.noreply.github.com>
2025-07-25 12:30:12 -0700

    add history timestamps back

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    set fzf_history_opts --with-nth=4.. # Exclude timestamps from history search
-
     direnv hook fish | source
 end
