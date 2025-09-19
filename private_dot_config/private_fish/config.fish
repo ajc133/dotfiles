eb95e6135416552cf1b110295cde89109abedc27
AJ Collins <16407457+ajc133@users.noreply.github.com>
2025-09-19 15:26:45 -0700

    feat: Add ctrl-t binding for fzf

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     direnv hook fish | source
+
+    # Ctrl + t to fuzzy search directory
+    fzf_configure_bindings --directory=\ct
 end
