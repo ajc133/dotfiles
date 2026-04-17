f018b26600009604c84f6200f84ea26a06c2f4cf
AJ Collins <16407457+ajc133@users.noreply.github.com>
2026-04-17 10:55:47 -0700

    set user paths in config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,11 @@
+set -g --export EDITOR nvim
+
+# "If an argument is not an existing directory, fish_add_path ignores it."
+fish_add_path $HOME/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path /opt/homebrew/bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
