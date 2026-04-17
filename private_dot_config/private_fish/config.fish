a0c40b982c59b20e3c4a0cfad1a5f1f0a1fc4f19
AJ Collins <16407457+ajc133@users.noreply.github.com>
2026-04-17 11:04:32 -0700

    move fish_add_path to interactive only

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,11 +1,5 @@
 set -g --export EDITOR nvim
 
-# "If an argument is not an existing directory, fish_add_path ignores it."
-fish_add_path $HOME/bin
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.cargo/bin
-fish_add_path /opt/homebrew/bin
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
@@ -13,4 +7,10 @@ if status is-interactive
 
     # Ctrl + t to fuzzy search directory
     fzf_configure_bindings --directory=\ct
+
+    # "If an argument is not an existing directory, fish_add_path ignores it."
+    fish_add_path $HOME/bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.cargo/bin
+    fish_add_path /opt/homebrew/bin
 end
