918d3e12ca47385d5269fc51286bc2098698f6e7
AJ Collins <16407457+ajc133@users.noreply.github.com>
2026-06-30 15:44:04 -0700

    add cargo run --bin

--- private_dot_config/private_fish/conf.d/abbr.fish
+++ private_dot_config/private_fish/conf.d/abbr.fish
@@ -1,9 +1,6 @@
 abbr -a --regex latest --position anywhere --function latest_download -- latest
-abbr --add sc sudo systemctl
-abbr --add scu systemctl --user
-abbr --add jc journalctl
-
-function last_history_item
-    echo $history[1]
-end
-abbr -a !! --position anywhere --function last_history_item
+abbr -a -- sc 'sudo systemctl'
+abbr -a -- scu 'systemctl --user'
+abbr -a -- jc journalctl
+abbr -a --position anywhere --function last_history_item -- !!
+abbr -a -- crb 'cargo run --bin'
